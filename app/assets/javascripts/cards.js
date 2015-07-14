961cfdc569482d83b9566841d08e4546abcfa0e0
Tim Won <tkw21189@gmail.com>
2015-07-14 17:39:19 -0500

    completed javascript matching logic

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -5,6 +5,9 @@ $(function(){
 		var need_uncheck = (e.target.id);
 		var need_first = ($('#first-selection').length === 0);
 		var need_second = ($('#second-selection').length === 0);
+
+
+		//determine which action to take
 		if (need_uncheck) {
 			$(e.target).removeAttr('id');
 		} else if (need_first){
@@ -12,39 +15,19 @@ $(function(){
 		} else if (need_second){
 			$(e.target).attr('id','second-selection');
 		}
+
+		//have two selections been made? 
+		var done = ($('#first-selection').length + $('#second-selection').length) == 2;
+
+		//check whether the IDs are a match
+		if (done){
+			alert($('#first-selection').html() === $('#second-selection').html());
+			reset();
+		}
 	});
 });
 
-/*$(function(){
-	$(".cards").on('click', 'li', function(e){
-		if (e.target.id =='first-selection') {
-			firstSelection(e);
-		}
-		else if($('#second-selection').length && $('#first-selection').length) {
-			return;
-		}
-		else if($('#first-selection').length) {
-			secondSelection(e);
-		}
-
-		else {
-			firstSelection(e);
-		}
-	});
-});*/
-
-function firstSelection(e){
-	if(e.target.id){
-		$(e.target).removeAttr('id');
-	} else {
-		$(e.target).attr('id','first-selection');
-	}
-}
-
-function secondSelection(e){
-	if(e.target.id){
-		$(e.target).removeAttr('id');
-	} else {
-		$(e.target).attr('id','second-selection');
-	}
+function reset(){
+	$('#first-selection').removeAttr('id','first-selection');
+	$('#second-selection').removeAttr('id','second-selection');
 }
